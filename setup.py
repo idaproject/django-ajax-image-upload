6244b99c549d05c7089928913f8c0f319fa37097
Alexander Temnikov <alexander.temnikov@me.com>
2018-01-29 10:38:20 +0300

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
-from setuptools import setup
+import re
+import sys
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
-readme = f.read()
-f.close()
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+def get_version(*file_paths):
+    """Retrieves the version from ajaximage/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+version = get_version("ajaximage", "__init__.py")
+
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='django-ajaximage',
-    version='0.2.8',
-    description='Upload images via ajax. Images are optionally resized.',
-    long_description=readme,
-    author="Bradley Griffiths",
-    author_email='bradley.griffiths@gmail.com',
-    url='https://github.com/bradleyg/django-ajaximage',
-    packages=['ajaximage'],
+    name='django-ajax-image-upload',
+    version=version,
+    description="""Upload images via ajax.""",
+    long_description=readme + '\n\n' + history,
+    author='Alexander Temnikov',
+    author_email='alexander.temnikov@me.com',
+    url='https://github.com/idaproject/django-ajax-image-upload',
+    packages=[
+        'ajaximage',
+    ],
     include_package_data=True,
-    install_requires=['Django', 'Pillow',],
+    install_requires=['Django>=1.8', read_file('requirements.txt')],
+    license="MIT",
     zip_safe=False,
+    keywords='django-ajax-image-upload',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
+        'Development Status :: 3 - Alpha',
         'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
