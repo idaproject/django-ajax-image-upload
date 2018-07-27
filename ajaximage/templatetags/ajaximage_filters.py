ea6ba2e5636f7c59e4ee8d6fcf0eb2aec6e665e9
Arseny Sysolyatin <arseny.sysolyatin@gmail.com>
2018-07-27 19:44:50 +0300

    bugfix2

--- ajaximage/templatetags/ajaximage_filters.py
+++ ajaximage/templatetags/ajaximage_filters.py
@@ -6,4 +6,8 @@ register = template.Library()
 @register.filter(name='upload_to')
 def upload_to(model):
     fields = model._meta.get_fields()
-    return next((field.upload_to for field in fields if hasattr(field, 'upload_to')), None)
+    field = next((field for field in fields if hasattr(field, 'upload_to')), None)
+    if callable(field.upload_to):
+        return field.upload_to('')
+    return field.upload_to
+
