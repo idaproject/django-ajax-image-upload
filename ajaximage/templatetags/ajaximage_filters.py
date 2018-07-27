52e09cf4dba7978cae0342fa32a420befdb177c6
Arseny Sysolyatin <arseny.sysolyatin@gmail.com>
2018-07-27 19:56:08 +0300

    bugfix4

--- ajaximage/templatetags/ajaximage_filters.py
+++ ajaximage/templatetags/ajaximage_filters.py
@@ -8,6 +8,6 @@ def upload_to(model):
     fields = model._meta.get_fields()
     field = next((field for field in fields if hasattr(field, 'upload_to')), None)
     if callable(field.upload_to):
-        return field.upload_to(None, '')
+        return field.upload_to(model, '')
     return field.upload_to
 
